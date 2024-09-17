1e5bfef615ddba352c2fa9c55da7487769fa51b3
Sazzad Hosen <108649437+Sazzad-Hosen-Mahim@users.noreply.github.com>
2024-09-17 20:27:08 +0600

    signed collection section completed

--- src/components/SignedCollection/SignedCollection.js
+++ src/components/SignedCollection/SignedCollection.js
@@ -1,8 +1,39 @@
 import React from "react";
 import "./SignedCollection.css";
+import ImgSignature from "../../assets/img/pat-signature White 1.png";
+import BookImage1 from "../../assets/img/img (1).png";
+import BookImage2 from "../../assets/img/img (8).png";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 const SignedCollection = () => {
-  return <div className="signed-collection-section"></div>;
+  return (
+    <section className="signed-collection-section">
+      <div className="signed-section-container">
+        <div className="article-container relative">
+          <div className="signed-heading-text">
+            <p className="white-para">PATRICK BET-DAVID'S </p>
+            <h1 className="red-para">
+              SIGNED <br />
+              COLLECTION
+            </h1>
+            <button className="signed-button">
+              SHOP NOW{" "}
+              <span className="inline-block ml-3 align-middle">
+                <FaArrowRightLong />
+              </span>
+            </button>
+          </div>
+          <div className="signature absolute">
+            <img src={ImgSignature} alt="" />
+          </div>
+        </div>
+        <div className="signature-image-container">
+          <img className="book-image" src={BookImage1} alt="" />
+          <img className="book-image2 mt-9" src={BookImage2} alt="" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default SignedCollection;
