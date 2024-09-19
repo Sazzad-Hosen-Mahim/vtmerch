ef7b26c86f916c8099b417652ff4e1d9cd99be08
Sazzad Hosen <108649437+Sazzad-Hosen-Mahim@users.noreply.github.com>
2024-09-20 05:11:31 +0600

    shop collection responsive except mobile responsive

--- src/components/ShopCollection/ShopCollection.js
+++ src/components/ShopCollection/ShopCollection.js
@@ -23,40 +23,36 @@ const ShopCollection = () => {
           </button>
           <div className="bag">
             <div className="bag-image">
-              <h1 className="uppercase common-shop-heading top-[436px]">
+              <h1 className="uppercase bag-image-heading">
                 The boss collection
               </h1>
             </div>
           </div>
           <div className="caps-books">
             <div className="caps-image">
-              <h1 className="uppercase common-shop-heading top-[195px]">
+              <h1 className="uppercase cap-image-heading">
                 head <br />
                 wear
               </h1>
             </div>
             <div className="books-image">
-              <h1 className="uppercase common-shop-heading top-[535px]">
-                value books
-              </h1>
+              <h1 className="uppercase books-image-heading">value books</h1>
             </div>
           </div>
         </div>
         <div className="bottle-image-container">
           <div className="bottle-image">
-            <h1 className="uppercase common-shop-heading top-[639px]">
-              Accessories
-            </h1>
+            <h1 className="uppercase bottle-image-heading">Accessories</h1>
           </div>
         </div>
         <div className="t-shirt-container ">
           <div className="t-shirt-one">
-            <h1 className="uppercase common-shop-heading top-[331px]">
+            <h1 className="uppercase t-shirt-one-heading">
               The Future Looks Bright Collection
             </h1>
           </div>
           <div className="t-shirt-two">
-            <h1 className="uppercase common-shop-heading top-[367px]">Tops</h1>
+            <h1 className="uppercase t-shirt-two-heading">Tops</h1>
           </div>
         </div>
       </div>
